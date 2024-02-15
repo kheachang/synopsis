09efdc480aa93adffa66f7fcbc724e99f107291a
Khea Chang <kheacyc@gmail.com>
2024-02-15 20:56:49 +0800

    rework algorithm with optimizations on the similarity function.

--- synopsis.py
+++ synopsis.py
@@ -4,47 +4,78 @@ from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from gensim.models import Word2Vec
 import numpy as np
-from scipy import spatial
 import networkx as nx
+from math import log10
 
 nltk.download("punkt")
 nltk.download('stopwords')
 
-with open("text.txt", "r", encoding="utf-8") as file:
-    text = file.read()
+def summarize(text, ratio=0.2, words=None, language="english", additional_stopwords=None):
+    if not isinstance(text, str):
+        raise ValueError("Text parameter must be a Unicode object (str)!")
 
-sentences = sent_tokenize(text)
+    # Tokenize sentences
+    sentences = sent_tokenize(text)
 
-sentences_clean = [re.sub(r"[^\w\s]", "", sentence.lower()) for sentence in sentences]
+    # Clean and tokenize words, removing stopwords
+    stop_words = stopwords.words(language)
+    if additional_stopwords:
+        stop_words.extend(additional_stopwords)
+    sentences_clean = [re.sub(r"[^\w\s]", "", sentence.lower()) for sentence in sentences]
+    sentence_tokens = [
+        [word for word in sentence.split(" ") if word not in stop_words]
+        for sentence in sentences_clean
+    ]
 
-stop_words = stopwords.words("english")
-sentence_tokens = [
-    [words for words in sentence.split(" ") if words not in stop_words]
-    for sentence in sentences_clean
-]
+    # Initialize Word2Vec model
+    w2v = Word2Vec(sentence_tokens, vector_size=100, min_count=1, epochs=1000)
 
-w2v = Word2Vec(sentence_tokens, vector_size=1, min_count=1, epochs=1000)
+    # Compute similarity matrix
+    similarity_matrix = np.zeros([len(sentences), len(sentences)])
+    for i, sentence_1 in enumerate(sentences):
+        for j, sentence_2 in enumerate(sentences):
+            if i != j:
+                similarity_matrix[i][j] = _get_similarity(sentence_1, sentence_2, w2v)
 
-sentence_embeddings=[[w2v.wv[word][0] for word in words] for words in sentence_tokens]
+    # Construct graph and calculate PageRank scores
+    nx_graph = nx.from_numpy_array(similarity_matrix)
+    scores = nx.pagerank(nx_graph)
 
-max_len=max([len(tokens) for tokens in sentence_tokens])
+    # Sort sentences by PageRank score
+    sorted_sentences = sorted(zip(sentences, scores.values()), key=lambda x: x[1], reverse=True)
 
-sentence_embeddings=[np.pad(embedding,(0,max_len-len(embedding)),'constant') for embedding in sentence_embeddings]
+    # Extract top sentences based on ratio or word count
+    if words:
+        top_sentences = [sentence for sentence, score in sorted_sentences[:words]]
+    else:
+        total_sentences = len(sorted_sentences)
+        top_sentences = [sentence for sentence, score in sorted_sentences[:int(total_sentences * ratio)]]
 
-similarity_matrix = np.zeros([len(sentence_tokens), len(sentence_tokens)])
+    return " ".join(top_sentences)
 
-for i,row_embedding in enumerate(sentence_embeddings):
-    for j,column_embedding in enumerate(sentence_embeddings):
-        similarity_matrix[i][j] = 1-spatial.distance.cosine(row_embedding,column_embedding)
+def _get_similarity(sentence_1, sentence_2, w2v):
+    words_sentence_one = sentence_1.split()
+    words_sentence_two = sentence_2.split()
 
-nx_graph = nx.from_numpy_array(similarity_matrix)
+    common_word_count = _count_common_words(words_sentence_one, words_sentence_two)
 
-scores = nx.pagerank(nx_graph)
+    log_s1 = log10(len(words_sentence_one))
+    log_s2 = log10(len(words_sentence_two))
 
-top_sentence = {sentence:scores[index] for index,sentence in enumerate(sentences)}
+    if log_s1 + log_s2 == 0:
+        return 0
 
-top = dict(sorted(top_sentence.items(), key=lambda x: x[1], reverse=True)[:4])
+    return common_word_count / (log_s1 + log_s2)
 
-for sent in sentences:
-    if sent in top.keys():
-        print(sent)
+def _count_common_words(words_sentence_one, words_sentence_two):
+    return len(set(words_sentence_one) & set(words_sentence_two))
+
+def main():
+    with open("text.txt", "r", encoding="utf-8") as file:
+        text = file.read()
+
+    summary = summarize(text)
+    print(summary)
+
+if __name__ == "__main__":
+    main()
