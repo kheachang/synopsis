6949178886215bb644d206089f8d6d5989d82891
Khea Chang <kheacyc@gmail.com>
2024-02-15 18:05:17 +0800

    calculate embedding for each word and replace word token with embedding in sentence_tokens

--- synopsis.py
+++ synopsis.py
@@ -1,7 +1,11 @@
 import nltk
-from nltk.tokenize import sent_tokenize
 import re
+from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
+from gensim.models import Word2Vec
+import numpy as np
+from scipy import spatial
+import networkx as nx
 
 nltk.download("punkt")
 
@@ -17,3 +21,12 @@ sentence_tokens = [
     [words for words in sentence.split(" ") if words not in stop_words]
     for sentence in sentences_clean
 ]
+
+w2v=Word2Vec(sentence_tokens,size=1,min_count=1,iter=1000)
+
+sentence_embeddings=[[w2v[word][0] for word in words] for words in sentence_tokens]
+
+max_len=max([len(tokens) for tokens in sentence_tokens])
+
+sentence_embeddings=[np.pad(embedding,(0,max_len-len(embedding)),'constant') for embedding in sentence_embeddings]
+
