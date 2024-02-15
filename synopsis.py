eec6b5319c856cd9fcde4e20a4dafda8e6c63ccc
Khea Chang <kheacyc@gmail.com>
2024-02-15 18:01:19 +0800

    remove punctuations and stopwords

--- synopsis.py
+++ synopsis.py
@@ -1,11 +1,19 @@
 import nltk
 from nltk.tokenize import sent_tokenize
+import re
+from nltk.corpus import stopwords
 
-nltk.download('punkt')
+nltk.download("punkt")
 
-with open('text.txt', 'r', encoding='utf-8') as file:
+with open("text.txt", "r", encoding="utf-8") as file:
     text = file.read()
 
 sentences = sent_tokenize(text)
 
+sentences_clean = [re.sub(r"[^\w\s]", "", sentence.lower()) for sentence in sentences]
 
+stop_words = stopwords.words("english")
+sentence_tokens = [
+    [words for words in sentence.split(" ") if words not in stop_words]
+    for sentence in sentences_clean
+]
